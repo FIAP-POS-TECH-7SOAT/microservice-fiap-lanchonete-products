82f67ef43fc99c044c338d422b770a2d8861bcb5
Thiago Rocha <tsrocha901@gmail.com>
2024-11-06 10:25:37 -0300

    feat: add info route

--- src/adapters/drivers/http/http.module.ts
+++ src/adapters/drivers/http/http.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 
 import { ProductController } from './controllers/product-controller';
+import { InfoController } from './controllers/info-controller';
 
 import DatabaseModule from '@adapters/drivens/infra/database/prisma/database.module';
 import { ProductModule } from '@core/modules/products/product.module';
 
 @Module({
   imports: [DatabaseModule, ProductModule],
-  controllers: [ProductController],
+  controllers: [ProductController, InfoController],
 })
 export class HTTPModule {}
