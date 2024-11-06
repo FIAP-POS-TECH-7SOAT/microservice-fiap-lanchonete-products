f0028a42235eac3ee93f5d05a6cbae70709b9ed0
Thiago Rocha <tsrocha901@gmail.com>
2024-11-06 11:28:17 -0300

    fix: fix prod start

--- src/adapters/drivens/infra/envs/env.ts
+++ src/adapters/drivens/infra/envs/env.ts
@@ -3,8 +3,6 @@ import { z } from 'zod';
 export const schemaEnv = z.object({
   DATABASE_URL: z.string().url(),
   PORT: z.coerce.number().optional().default(3000),
-  JWT_PRIVATE_KEY: z.string(),
-  JWT_PUBLIC_KEY: z.string(),
 });
 
 export type Env = z.infer<typeof schemaEnv>;
